74bf4bdaf2e869765a32640d12d8c2d5fb2069fc
Nate <codebynate@gmail.com>
2025-03-12 21:30:43 +0000

    Create form

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,49 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <h1>BMI Calculator</h1>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <h1>BMI Calculator</h1>
+            </header>
+
+            <main>
+                <section className="calculator">
+                    <h2>Enter your details below</h2>
+
+                    <form>
+                        <div className="units">
+                            <div>
+                                <input type="radio" name="units" id="metric" />
+                                <label htmlFor="metric">Metric</label>
+                            </div>
+
+                            <div>
+                                <input type="radio" name="units" id="imperial" />
+                                <label htmlFor="imperial">Imperial</label>
+                            </div>
+                        </div>
+
+                        <div className="measurements">
+                            <div>
+                                <label htmlFor="height">Height</label>
+                                <input type="text" name="height" id="height" />
+                            </div>
+
+                            <div>
+                                <label htmlFor="weight">Weight</label>
+                                <input type="text" name="weight" id="weight" />
+                            </div>
+                        </div>
+
+                        <button className="calculate-button" type="submit">Calculate</button>
+                    </form>
+                </section>
+            </main>
+
+            <footer></footer>
+        </>
+    )
 }
 
 export default App
