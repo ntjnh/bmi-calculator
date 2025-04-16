517a19f8c4b9b697edb96473ac397fd1b71b2ff1
Nate <codebynate@gmail.com>
2025-04-16 16:46:21 +0100

    Add to calculate test to check for height or weight of 0

--- tests/unit/calculate.test.js
+++ tests/unit/calculate.test.js
@@ -7,8 +7,13 @@ test('calculates a BMI of 21.6 from 52kg weight and 1.55m height', () => {
 })
 
 // Imperial units
-test('calculates a BMI of 22.3 from 173.5lbs weight and 6ft 2in height', () => {
+test('calculates a BMI of 22.3 from 12st 5.5lbs weight and 6ft 2in height', () => {
     const height = imperialHeight(6, 2)
-    const weight = imperialWeight(173.5)
-    expect(calculate(height, weight))
+    const weight = imperialWeight(12, 5.5)
+    expect(calculate(height, weight)).toBe(22.3)
+})
+
+// Refuse height or weight of 0
+test('gives an error if no height or weight has been entered', () => {
+    expect(() => calculate(0, 0)).toThrowError('Invalid height or weight')
 })
