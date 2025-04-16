991bc63eb009cc80db038e8449da5cdbd63faaeb
Nate <codebynate@gmail.com>
2025-04-16 11:59:10 +0100

    Add comment to calculate func

--- src/lib/calculate.js
+++ src/lib/calculate.js
@@ -1,6 +1,6 @@
 // Calculate BMI - weight in kg divided by height in metres squared
 export default function calculate(height, weight) {
-    height *= .01
+    height *= .01 // convert cm to m first
     const bmi = weight / Math.pow(height, 2)
     return Number(bmi.toFixed(1))
 }
