59d0ad4ffd3ace9de6bb53405c630875fd12fd2b
Nate <codebynate@gmail.com>
2025-04-16 12:02:00 +0100

    Update unit test for imperial func

--- tests/unit/imperial.test.js
+++ tests/unit/imperial.test.js
@@ -1,10 +1,10 @@
 import { expect, test } from 'vitest'
 import { imperialHeight, imperialWeight } from '../../src/lib/imperial'
 
-test('converts height from 6ft 2in to 1.88m', () => {
-    expect(imperialHeight(6, 2)).toBe(1.88)
+test('converts height from 6ft 2in to 188cm', () => {
+    expect(imperialHeight(6, 2)).toBe(188)
 })
 
-test('converts weight from 160 lbs to 72.56kg', () => {
-    expect(imperialWeight(160)).toBe(72.56)
+test('converts weight from 11st 5lbs to 72.11kg', () => {
+    expect(imperialWeight(11, 5)).toBe(72.11)
 })
