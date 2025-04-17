284611211b97e4445fa84fc2ebabbefa1548d457
Nate <codebynate@gmail.com>
2025-04-17 15:52:19 +0100

    Add cm to m conversion to range func

--- src/lib/range.js
+++ src/lib/range.js
@@ -1,5 +1,6 @@
 // Calculate healthy weight range
 export default function range(height) {
+    height *= .01 // convert cm to m first
     const min = Number((Math.pow(height, 2) * 18.5).toFixed(2))
     const max = Number((Math.pow(height, 2) * 24.9).toFixed(2))
     return [min, max]
