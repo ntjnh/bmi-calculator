663fe8292253cc0831b5bf5c7b63fccdd39d12bc
Nate <codebynate@gmail.com>
2025-04-17 15:51:49 +0100

    Update imperial funcs

--- src/lib/imperial.js
+++ src/lib/imperial.js
@@ -1,11 +1,11 @@
 export function imperialHeight(ft, ins) {
-    const inches = ft * 12 + ins
+    const inches = (ft * 12) + ins
     const centimetres = inches * 2.54
     return Math.round(centimetres)
 }
 
 export function imperialWeight(st, lbs) {
-    const pounds = st * 14 + lbs
+    const pounds = (st * 14) + lbs
     const kilograms = pounds / 2.205
-    return Number(kilograms.toFixed(2))
+    return Math.round(kilograms)
 }
