3ef027cc43e817315565274944f6e79dbe5bff77
Nate <codebynate@gmail.com>
2025-04-23 17:41:02 +0100

    Amend calculate tests syntax and comments

--- tests/unit/calculate.test.js
+++ tests/unit/calculate.test.js
@@ -2,15 +2,16 @@ import { expect, test } from 'vitest'
 import calculate from '../../src/lib/calculate'
 import { imperialHeight, imperialWeight } from '../../src/lib/imperial'
 
-test('calculates a BMI of 21.6 from 52kg weight and 1.55m height', () => {
+// Metric units
+test('calculates a BMI of 21.6 from 52kg weight and 155cm height', () => {
     expect(calculate(155, 52)).toBe(21.6)
 })
 
 // Imperial units
-test('calculates a BMI of 22.3 from 12st 5.5lbs weight and 6ft 2in height', () => {
+test('calculates a BMI of 22.2 from 12st 5lbs weight and 6ft 2in height', () => {
     const height = imperialHeight(6, 2)
-    const weight = imperialWeight(12, 5.5)
-    expect(calculate(height, weight)).toBe(22.3)
+    const weight = imperialWeight(12, 5)
+    expect(calculate(height, weight)).toBe(22.2)
 })
 
 // Refuse height or weight of 0
