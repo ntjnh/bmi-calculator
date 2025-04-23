f22cea18182610b16c1b7105d0d6c8d7ea6beff5
Nate <codebynate@gmail.com>
2025-04-23 17:55:40 +0100

    Change range func test case to cm instead of m

--- tests/unit/range.test.js
+++ tests/unit/range.test.js
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import range from '../../src/lib/range'
 
-test('calculates healthy weight range for height of 1.55m', () => {
-    expect(range(1.55)).toStrictEqual([44.45, 59.82])
+test('calculates healthy weight range for height of 155cm', () => {
+    expect(range(155)).toStrictEqual([44.45, 59.82])
 })
