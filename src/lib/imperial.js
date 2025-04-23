b937c96ca0c10a07a286b61d687a14114c891839
Nate <codebynate@gmail.com>
2025-04-23 17:42:19 +0100

    Fix imperial funcs by converting args to numbers

--- src/lib/imperial.js
+++ src/lib/imperial.js
@@ -1,11 +1,11 @@
 export function imperialHeight(ft, ins) {
-    const inches = (ft * 12) + ins
+    const inches = parseInt(ft, 10) * 12 + parseInt(ins, 10)
     const centimetres = inches * 2.54
     return Math.round(centimetres)
 }
 
 export function imperialWeight(st, lbs) {
-    const pounds = (st * 14) + lbs
+    const pounds = parseInt(st, 10) * 14 + parseInt(lbs, 10)
     const kilograms = pounds / 2.205
-    return Math.round(kilograms)
+    return Math.round(kilograms * 100) / 100
 }
