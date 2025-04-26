80d3453a863200babe0ef70ecb543f7e8a99f167
Nate <codebynate@gmail.com>
2025-04-26 18:11:00 +0100

    Write test to check blank height or weight metric field errors

--- tests/e2e/form-validation.spec.jsx
+++ tests/e2e/form-validation.spec.jsx
@@ -1,40 +1,39 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { render } from 'vitest-browser-react'
 import App from '../../src/App'
 
-test('displays appropriate error messages for invalid inputs', async () => {
-    const screen = render(<App />)
-
-    const metric = screen.getByLabelText('Metric')
-    const cm = screen.getByLabelText('Centimetres')
-    const kg = screen.getByLabelText('Kilograms')
-    const calcButton = screen.getByRole('button').getByText('Calculate')
-
-    const errorMessage = screen.getByRole('alert')
-
-    await metric.click()
-
-    await cm.fill('155')
-    // await kg.fill('52')
-
-    // Calculate the BMI
-    await calcButton.click()
-
-    /* Check error handling */
-    await expect.element(kg).not.toHaveValue()
-
-    await expect.element(errorMessage).toBeInTheDocument()
-    await expect.element(errorMessage).toHaveTextContent('Weight cannot be blank.')
-    await expect.element(errorMessage).toHaveClass('error-msg')
-    await expect.element(errorMessage).toHaveStyle({
-        color: 'oklch(0.505 0.213 27.518)',
-        fontWeight: '600'
+describe.each([
+    { filled: 'Centimetres', notFilled: 'Kilograms', val: '155', field: 'Weight' },
+    { filled: 'Kilograms', notFilled: 'Centimetres', val: '52', field: 'Height' }
+])('calculator error handling ', ({ filled, notFilled, val, field }) => {
+    test(`displays appropriate error messages when ${field} is not filled in`, async () => {
+        const screen = render(<App />)
+    
+        const metric = screen.getByLabelText('Metric')
+        const calcButton = screen.getByRole('button').getByText('Calculate')
+    
+        const errorMessage = screen.getByRole('alert')
+    
+        await metric.click()
+    
+        await screen.getByLabelText(filled).fill(val)
+    
+        await calcButton.click()
+    
+        /* Check error handling */
+        await expect.element(screen.getByLabelText(notFilled)).not.toHaveValue()
+    
+        await expect.element(errorMessage).toBeInTheDocument()
+        await expect.element(errorMessage).toHaveTextContent(`${field} cannot be blank.`)
+        await expect.element(errorMessage).toHaveClass('error-msg')
+        await expect.element(errorMessage).toHaveStyle({
+            color: 'oklch(0.505 0.213 27.518)',
+            fontWeight: '600'
+        })
+    
+        // Error styling on the specific field
+        await expect.element(screen.getByLabelText(notFilled)).toHaveStyle({
+            borderColor: 'oklch(0.505 0.213 27.518)'
+        })
     })
-
-    // Error styling on specific field
-    await expect.element(kg).toHaveStyle({
-        borderColor: 'oklch(0.505 0.213 27.518)'
-    })
-
-    // Error styling and text should be gone after resubmitting
 })
