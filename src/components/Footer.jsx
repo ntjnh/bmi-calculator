ccd55e89d5a54bac497a46725f1bd554f5b74ead
Nate <codebynate@gmail.com>
2025-05-03 16:51:12 +0100

    Update footer to match other projects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 export default function Footer() {
-    const dev = 'NATE.DEV'
-    const today = new Date()
-    const year = today.getFullYear()
-
     return (
-        <footer className="pb-4 md:py-6 xl:pt-0 text-center">© {dev} {year}</footer>
+        <footer className="pb-4 md:py-6 xl:pt-4 text-center text-sm">
+            <a
+                className="text-teal-800 underline hover:text-teal-950"
+                href="https://nate-dev.com?utm_source=codebynate&utm_medium=web&utm_campaign=next_bank_hol"
+                target="_blank"
+                rel="noreferrer"
+            >Nate</a> built this.
+        </footer>
     )
 }
